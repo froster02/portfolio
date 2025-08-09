f9f8d991a187a5e0cff641a26f734f760d1d4104
Arush Naudiyal <arushnaudiyal@gmail.com>
2025-08-10 00:12:08 +0530

    Implement initial project structure and setup

---
+++ config.js
@@ -0,0 +1,60 @@
+// Configure your site here. No build step needed.
+// Set your GitHub username to auto-populate profile and projects.
+
+window.siteConfig = {
+  // REQUIRED: set this to your GitHub username
+  githubUsername: "froster02",
+
+  // Optional overrides; will fall back to GitHub profile when available
+  name: "Arush Naudiyal",
+  tagline: "A Passionate Software Engineer from India",
+  email: "arushnaudiyal@gmail.com",
+  website: "",
+  twitter: "",
+  linkedin: "https://www.linkedin.com/in/arush-naudiyal-8886a387/",
+  resumeUrl: "https://drive.google.com/file/d/1Fosbz0qRBEQVugJXmeEZwtbDiwOWYfYo/view?usp=sharing",
+
+  // LeetCode / WakaTime / Stats handles
+  leetcode: {
+    username: "froster02",
+    solved: 310,
+    submissions: 830,
+    profileUrl: "https://leetcode.com/u/froster02/"
+  },
+  wakatime: {
+    userId: "13cdbe77-08e8-4c8f-b3d7-2f0eb5a8923d",
+    username: "froster02"
+  },
+
+  // Optionally pin specific repositories first
+  featuredRepos: [
+    "reactJS", // React textutility app (as per your list)
+    "movies-recommendation-system", // Python movie recommendation app
+    "DS_Algo" // Learning take CP
+  ],
+
+  // Skills icons (label + icon URL)
+  skills: [
+    { label: "C", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg" },
+    { label: "C++", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/cplusplus/cplusplus-original.svg" },
+    { label: "Java", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg" },
+    { label: "JavaScript", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" },
+    { label: "ReactJS", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg" },
+    { label: "Python", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg" },
+    { label: "Flask", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flask/flask-original.svg" },
+    { label: "Spring", icon: "https://www.vectorlogo.zone/logos/springio/springio-icon.svg" },
+    { label: "Firebase", icon: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg" },
+    { label: "MySQL", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg" },
+    { label: "MongoDB", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg" },
+    { label: "Git", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg" },
+    { label: "GitHub", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/github/github-original.svg" },
+    { label: "Android", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original.svg" },
+    { label: "HTML5", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg" },
+    { label: "CSS3", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg" },
+    { label: "Bootstrap", icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain.svg" },
+    { label: "Jenkins", icon: "https://www.vectorlogo.zone/logos/jenkins/jenkins-icon.svg" },
+  ],
+
+  // Number of projects to show
+  projectsLimit: 9
+};
